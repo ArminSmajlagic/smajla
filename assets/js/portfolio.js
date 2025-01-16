e5b21c3131db2d89dc1edd3b319849f474d6a3a8
Armin Smajlagic <45321513+ArminSmajlagic@users.noreply.github.com>
2025-01-16 14:06:36 +0100

    Mailing

---
+++ assets/js/portfolio.js
@@ -0,0 +1,54 @@
+function onLoadSite(){
+    (function() {
+        // https://dashboard.emailjs.com/admin/account
+        emailjs.init({
+          publicKey: "hCoA1Ola99PM7D7C1",
+        });
+    })();
+    
+    document.getElementById("contact").onsubmit = function() {
+        sendEmail();
+    };
+    
+    console.log("Site loaded")
+}
+
+function sendEmail(event) {
+
+    console.log("sendEmail called")
+    event.preventDefault(); // Prevent the default form submission
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const body = document.getElementById('comment').value;
+
+    const templateParams = {
+        email: email,
+        to_name: name,
+        message: "I am glad you contacted me. You will soon get response from me so we can talk and I can send you my full CV and portfolio. \nThis is automated response.",
+    };
+
+    emailjs.send('service_fe0b3j7', 'template_i7vts0h', templateParams)
+        .then((response) => {
+            alert('Email sent successfully!');
+        }, (error) => {
+            alert('Failed to send email. Please try again later.');
+        });
+
+    //to me
+    
+    const templateParams1 = {
+        email: "smajlagicarmin@gmail.com",
+        to_name: "Armin",
+        message: "Mail from " + name + " was sent with body "+body,
+    };
+
+    emailjs.send('service_fe0b3j7', 'template_i7vts0h', templateParams1)
+        .then((response) => {
+            alert('Email sent successfully!');
+        }, (error) => {
+            alert('Failed to send email. Please try again later.');
+        });
+}
+
+onLoadSite();
